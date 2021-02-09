57ab980bd51014e087ebb22fe0dbdf5524aee34c
Christophe Hurpeau <christophe@hurpeau.com>
2021-02-09 16:09:54 +0100

    feat(eslint-config): add config for rollup

---
+++ @ornikar/eslint-config/rollup.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// See https://github.com/ornikar/shared-configs/tree/master/%40ornikar/rollup-config
+module.exports = {
+  globals: {
+    __DEV__: true,
+    __TARGET__: true,
+  },
+};
