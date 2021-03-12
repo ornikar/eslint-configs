0d2a3245139ddf05b5b1e2c94685e3da6a2dcd40
Christophe Hurpeau <christophe@hurpeau.com>
2021-03-12 13:07:19 +0100

    feat: remove max-lines rule [no issue] (#17)

--- @ornikar/eslint-config/rules/code-quality.js
+++ @ornikar/eslint-config/rules/code-quality.js
@@ -3,21 +3,5 @@
 module.exports = {
   rules: {
     'max-depth': ['warn', 6],
-    'max-lines': [
-      'warn',
-      {
-        max: 120,
-        skipBlankLines: false,
-        skipComments: false,
-      },
-    ],
   },
-  overrides: [
-    {
-      files: ['stories.js'],
-      rules: {
-        'max-lines': 'off',
-      },
-    },
-  ],
 };
