98e76a7be14518e07ab55809280d54ad35b9717f
Debove Christopher <chibiblasphem@gmail.com>
2021-09-21 11:11:35 +0200

    feat: enable @typescript-eslint/prefer-ts-expect-error (#91)

---
+++ @ornikar/eslint-config-typescript/tests/prefer-ts-expect-error.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error, @typescript-eslint/ban-ts-comment
+// @ts-ignore I want to ignore
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const a: string = 21;
+
+// @ts-expect-error I want to ignore
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const b: string = 21;
