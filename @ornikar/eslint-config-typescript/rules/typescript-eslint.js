fb45958e273bc598c6fb96c031fa3ced5913db19
Christopher Debove <chibiblasphem@gmail.com>
2021-11-15 11:19:43 +0100

    feat: set uprefer-switch rule

--- @ornikar/eslint-config-typescript/rules/typescript-eslint.js
+++ @ornikar/eslint-config-typescript/rules/typescript-eslint.js
@@ -21,6 +21,7 @@ module.exports = {
     '@typescript-eslint/prefer-ts-expect-error': 'error',
     '@typescript-eslint/method-signature-style': 'error',
     '@typescript-eslint/prefer-string-starts-ends-with': 'error',
+    '@typescript-eslint/switch-exhaustiveness-check': 'error',
 
     /* Enabled as 'warn' in recommended, changed to 'error' */
     '@typescript-eslint/no-non-null-assertion': 'error',
