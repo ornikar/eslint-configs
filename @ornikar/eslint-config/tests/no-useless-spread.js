bbb279d787110e663289634c48a855d3f5d83195
Lucas Delon <lucas.delon@ornikar.com>
2021-12-21 11:17:26 +0100

    feat(eslint-config): fix no-unused-vars [no issue]

--- @ornikar/eslint-config/tests/no-useless-spread.js
+++ @ornikar/eslint-config/tests/no-useless-spread.js
@@ -4,14 +4,14 @@ const firstElement = 'foo';
 const secondElement = 'bar';
 const thirdElement = 'baz';
 
-// eslint-disable-next-line no-unused-vars, unicorn/no-useless-spread
+// eslint-disable-next-line unicorn/no-useless-spread
 const array = [firstElement, ...[secondElement], thirdElement];
 
 const firstProperty = 'foo';
 const secondProperty = 'bar';
 const thirdProperty = 'baz';
 
-// eslint-disable-next-line no-unused-vars, unicorn/no-useless-spread
+// eslint-disable-next-line unicorn/no-useless-spread
 const object = { firstProperty, ...{ secondProperty }, thirdProperty };
 
 const iterable = ['foo', 'bar', 'baz'];
@@ -19,14 +19,16 @@ const iterable = ['foo', 'bar', 'baz'];
 // eslint-disable-next-line unicorn/no-useless-spread
 const set = new Set([...iterable]);
 
-// // eslint-disable-next-line no-unused-vars, unicorn/no-useless-spread
-// const results = await Promise.all([...iterable]);
-
 // eslint-disable-next-line no-unused-vars, no-restricted-syntax, unicorn/no-useless-spread
 for (const foo of [...set]);
 
-// eslint-disable-next-line no-unused-vars
 const validArray = [firstElement, secondElement, thirdElement];
 
-// eslint-disable-next-line no-unused-vars
 const validObject = { firstProperty, secondProperty, thirdProperty };
+
+exports = {
+  array,
+  object,
+  validArray,
+  validObject,
+};
