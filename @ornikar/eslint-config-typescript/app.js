bfaa5bc3d1d9d2ae6a1f3fccd9536541912434b6
Christophe Hurpeau <christophe@hurpeau.com>
2022-02-21 15:05:49 +0100

    feat: add app configs

---
+++ @ornikar/eslint-config-typescript/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  rules: {
+    /* No need for this rule in an app */
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+    /* Allow camelcase coming from api */
+    camelcase: 'off',
+  },
+};
