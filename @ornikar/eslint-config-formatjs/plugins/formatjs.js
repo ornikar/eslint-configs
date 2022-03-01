8c76f1a1fca01d49740f527be0dd6ae26388cff3
Christophe Hurpeau <christophe@hurpeau.com>
2022-03-01 10:13:02 +0100

    feat: set all rules, enabled or not

--- @ornikar/eslint-config-formatjs/plugins/formatjs.js
+++ @ornikar/eslint-config-formatjs/plugins/formatjs.js
@@ -3,7 +3,17 @@
 module.exports = {
   plugins: ['formatjs'],
   rules: {
+    'formatjs/blocklist-elements': 'off',
+    'formatjs/enforce-description': 'off',
     'formatjs/enforce-placeholders': 'error',
+    'formatjs/enforce-plural-rules': 'error',
+    'formatjs/enforce-default-message': 'error',
+    'formatjs/no-camel-case': 'off',
     'formatjs/no-emoji': 'error',
+    'formatjs/no-multiple-whitespaces': 'error',
+    'formatjs/no-multiple-plurals': 'error',
+    'formatjs/no-offset': 'error',
+    'formatjs/enforce-id': 'error',
+    'formatjs/no-complex-selectors': 'off',
   },
 };
