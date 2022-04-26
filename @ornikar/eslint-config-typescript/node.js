481cf34f221138b5c6691dc9031571c34f703651
Christophe Hurpeau <christophe@hurpeau.com>
2022-04-26 11:32:58 +0200

    feat(eslint-config-typescript): disable node/no-unsupported-features for typescript

--- @ornikar/eslint-config-typescript/node.js
+++ @ornikar/eslint-config-typescript/node.js
@@ -2,4 +2,10 @@
 
 module.exports = {
   extends: ['@ornikar/eslint-config/node', './_shared', '@ornikar/eslint-config/rules/prettier'].map(require.resolve),
+
+  rules: {
+    // disable no-unsupported-features when using babel
+    'node/no-unsupported-features': 'off',
+    'node/no-unsupported-features/es-syntax': 'off',
+  },
 };
--- @ornikar/eslint-config/rules/node.js
+++ @ornikar/eslint-config/rules/node.js
@@ -3,6 +3,7 @@
 module.exports = {
   plugins: ['node'],
   extends: ['plugin:node/recommended', require.resolve('./node-override')],
+
   parserOptions: {
     // top level await is introduced in ecmaVersion: 2022 but supported since node 14
     ecmaVersion: 2022,
