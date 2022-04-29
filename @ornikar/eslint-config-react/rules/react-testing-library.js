b399b964d36538e290eded32ec9ef25b931e8666
PSniezak <paulsniezak@gmail.com>
2022-04-29 18:20:59 +0200

    feat(rules): add testing-library/no-wait-for-multiple-assertions

--- @ornikar/eslint-config-react/rules/react-testing-library.js
+++ @ornikar/eslint-config-react/rules/react-testing-library.js
@@ -13,5 +13,6 @@ module.exports = {
     'testing-library/no-promise-in-fire-event': 'error',
     'testing-library/no-render-in-setup': 'error',
     'testing-library/no-unnecessary-act': 'error',
+    'testing-library/no-wait-for-multiple-assertions': 'error',
   },
 };
---
+++ @ornikar/eslint-config-react/tests/react-testing-library/no-wait-for-multiple-assertions.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+
+// Incorrect uses
+const foo = async () => {
+  await waitFor(() => {
+    expect(a).toEqual('a');
+    // eslint-disable-next-line testing-library/no-wait-for-multiple-assertions
+    expect(b).toEqual('b');
+  });
+
+  // or
+  await waitFor(() => {
+    expect(a).toEqual('a');
+    // eslint-disable-next-line testing-library/no-wait-for-multiple-assertions
+    expect(b).toEqual('b');
+  });
+};
+
+// Correct uses
+
+const foo2 = async () => {
+  await waitFor(() => expect(a).toEqual('a'));
+  expect(b).toEqual('b');
+
+  await waitFor(() => {
+    expect(a).toEqual('a');
+  });
+  expect(b).toEqual('b');
+
+  await waitFor(() => {
+    fireEvent.keyDown(input, { key: 'ArrowDown' });
+    expect(b).toEqual('b');
+  });
+};
