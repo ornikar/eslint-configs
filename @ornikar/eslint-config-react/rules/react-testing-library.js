680805a8090fc086e03a1b30f52431042b060ead
PSniezak <paulsniezak@gmail.com>
2022-04-29 17:43:04 +0200

    feat(rules): add testing-library/no-container

--- @ornikar/eslint-config-react/rules/react-testing-library.js
+++ @ornikar/eslint-config-react/rules/react-testing-library.js
@@ -5,5 +5,6 @@ module.exports = {
   rules: {
     'testing-library/await-async-query': 'error',
     'testing-library/no-manual-cleanup': 'error',
+    'testing-library/no-container': 'error',
   },
 };
