143cc0877158ca22fc3587aa7cc2ad635d649d97
Lucas Delon <lucas.delon@ornikar.com>
2022-05-02 17:37:45 +0200

    feat(eslint-config): fix rule unicorn/prefer-modern-math-apis [no issue]

--- @ornikar/eslint-config/tests/prefer-modern-math-apis.js
+++ @ornikar/eslint-config/tests/prefer-modern-math-apis.js
@@ -4,17 +4,17 @@ const bar = 0;
 
 // Incorrect uses
 
-// eslint-disable-next-line no-unused-expressions, unicorn/prefer-modern-math-apis
-Math.log(bar) * Math.LOG10E;
+// eslint-disable-next-line unicorn/prefer-modern-math-apis
+exports.foo = Math.log(bar) * Math.LOG10E;
 
-// eslint-disable-next-line no-unused-expressions, unicorn/prefer-modern-math-apis
-Math.LOG2E * Math.log(bar);
+// eslint-disable-next-line unicorn/prefer-modern-math-apis
+exports.foo = Math.LOG2E * Math.log(bar);
 
-// eslint-disable-next-line no-unused-expressions, unicorn/prefer-modern-math-apis
-Math.log(bar) / Math.LN10;
+// eslint-disable-next-line unicorn/prefer-modern-math-apis
+exports.foo = Math.log(bar) / Math.LN10;
 
-// eslint-disable-next-line no-unused-expressions, unicorn/prefer-modern-math-apis
-Math.log(bar) / Math.LN2;
+// eslint-disable-next-line unicorn/prefer-modern-math-apis
+exports.foo = Math.log(bar) / Math.LN2;
 
 // Correct uses
 
