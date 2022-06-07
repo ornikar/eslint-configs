7b58b1b986f6831ad95b93993282a758335c0dc4
Thomas Simonnet <thomas.simonnet@ornikar.com>
2022-06-07 16:21:51 +0200

    feat(naming): allow non prefixed destructured booleans and snake case… [no issue] (#323)
    * feat(naming): allow non prefixed destructured booleans and snake case properties with prefix
    
    * fix: pr revie
    
    * fix: pr review
    
    * Update @ornikar/eslint-config-typescript/rules/typescript-eslint.js
    
    Co-authored-by: Christophe Hurpeau <christophe@hurpeau.com>
    
    Co-authored-by: Christophe Hurpeau <christophe@hurpeau.com>

--- @ornikar/eslint-config-typescript/tests/naming-convention.ts
+++ @ornikar/eslint-config-typescript/tests/naming-convention.ts
@@ -2,6 +2,15 @@ export const isBooleanVariable = false;
 // warning eslint
 export const invalidIsBooleanVariable = false;
 
+export const validObject = { isLoading: true };
+// warning eslint
+const invalidObject = { loading: true };
+
+export const { loading } = invalidObject;
+export const { loading: isLoading } = invalidObject;
+// warning eslint
+export const { loading: invalidIsLoading } = invalidObject;
+
 export interface ExampleInterface {
   isBooleanVariable: boolean;
   $isBooleanVariable: boolean;
@@ -11,6 +20,14 @@ export interface ExampleInterface {
 
 export function exampleFunction(
   isValidBooleanParameter: boolean,
+  $isValidBooleanParameter: boolean,
   // warning eslint
   invalidBooleanParameter: boolean,
 ): void {}
+
+export const validSnakeCaseObject = {
+  is_valid_variable: true,
+  can_be_valid: true,
+  isvalid_variable: true,
+  valid_variable: true,
+};
