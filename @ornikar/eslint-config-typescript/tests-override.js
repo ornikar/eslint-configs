47d8917e5f717f04a586774f08a1a894db114d3e
PSniezak <paulsniezak@gmail.com>
2022-06-22 15:18:24 +0200

    feat(eslint-config-typescript): restrict act imports

--- @ornikar/eslint-config-typescript/tests-override.js
+++ @ornikar/eslint-config-typescript/tests-override.js
@@ -2,4 +2,19 @@
 
 module.exports = {
   extends: ['@ornikar/eslint-config/tests-override'].map(require.resolve),
+
+  rules: {
+    'no-restricted-imports': [
+      'error',
+      {
+        paths: [
+          {
+            name: 'react-test-renderer',
+            importNames: ['act'],
+            message: 'Please use act from @testing-library/* instead.',
+          },
+        ],
+      },
+    ],
+  },
 };
