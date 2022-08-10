cf8403bc511b5a15fa2cf1dea8045529fc0a49bc
Lucas Delon <lucas.delon@ornikar.com>
2022-08-10 13:54:20 +0200

    feat(jsx-curly-brace-presence): add eslint-disable [no issue]

--- @ornikar/eslint-config-react/tests/JSXQuotes.js
+++ @ornikar/eslint-config-react/tests/JSXQuotes.js
@@ -8,7 +8,9 @@ function Color() {
 
 export function JSXQuotes() {
   return (
+    // eslint-disable-next-line react/jsx-curly-brace-presence
     <App prop={'Hello world'}>
+      {/* eslint-disable-next-line react/jsx-curly-brace-presence */}
       {'Hello world'}
       <Color text={'\u00A0'} />
       {'Hello \u00B7 world'};<style type="text/css">{'.main { margin-top: 0; }'}</style>;
