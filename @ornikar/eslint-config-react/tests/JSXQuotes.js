d7ee70a67c5928f707e04e9f4df243d4cf7fb4f8
Christophe Hurpeau <christophe@hurpeau.com>
2022-08-31 09:46:14 +0200

    test: add tests for unescaped entities

--- @ornikar/eslint-config-react/tests/JSXQuotes.js
+++ @ornikar/eslint-config-react/tests/JSXQuotes.js
@@ -8,12 +8,18 @@ function Color() {
 
 export function JSXQuotes() {
   return (
-    // eslint-disable-next-line react/jsx-curly-brace-presence
-    <App prop={'Hello world'}>
+    <App
+      validProp="Hello world"
+      // eslint-disable-next-line react/jsx-curly-brace-presence
+      invalidProp={'Hello world'}
+    >
+      Hello World
       {/* eslint-disable-next-line react/jsx-curly-brace-presence */}
       {'Hello world'}
       <Color text={'\u00A0'} />
       {'Hello \u00B7 world'};<style type="text/css">{'.main { margin-top: 0; }'}</style>;
+      <p>A paragraph without "base" prop set should be displayed as body.</p>
+      <div> {'>'} </div>
     </App>
   );
 }
