fd934bb0d3dc61d9df8b582e7fe37c54d1f2ca39
Lucas Delon <lucas.delon@ornikar.com>
2022-11-04 16:30:24 +0100

    feat(eslint-config): add no unsafe declaration merging rule [no issue]

--- @ornikar/eslint-config-typescript/rules/typescript-eslint.js
+++ @ornikar/eslint-config-typescript/rules/typescript-eslint.js
@@ -92,6 +92,7 @@ module.exports = {
     '@typescript-eslint/no-unsafe-call': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
     '@typescript-eslint/no-unsafe-return': 'off',
+    '@typescript-eslint/no-unsafe-declaration-merging': 'error',
 
     /* Naming convention */
     '@typescript-eslint/naming-convention': [
