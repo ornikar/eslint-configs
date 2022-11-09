975dba122230ffc434e5a078931310664f4f4513
Pierre Touré <pierre.toure@ornikar.com>
2022-11-09 17:58:25 +0100

    refactor: move prefer-export-from tests

--- @ornikar/eslint-config/tests/node-module/prefer-export-from/prefer-export-from.js
+++ @ornikar/eslint-config/tests/node-module/prefer-export-from/prefer-export-from.js
@@ -1,22 +1,25 @@
-/* eslint-disable import/no-unresolved, import/extensions, import/order, import/no-duplicates */
-
-import { namedFoo } from './foo';
-import * as namespaceFoo from './foo';
-import defaultExport, { namedBar } from './foo';
-import * as namespaceBaz from './foo';
+/* eslint-disable import/no-unresolved, import/extensions, import/order, import/first, simple-import-sort/exports, import/newline-after-import, import/no-duplicates */
 
 // Incorrect uses
 
+import { namedFoo } from './foo';
 // eslint-disable-next-line unicorn/prefer-export-from
 export { namedFoo };
+
+import * as namespaceFoo from './foo';
 // eslint-disable-next-line unicorn/prefer-export-from
 export { namespaceFoo };
+
+import defaultExport, { namedBar } from './foo';
 // eslint-disable-next-line unicorn/prefer-export-from
 export { namedBar, defaultExport as renamedDefault, namedBar as renamedNamed };
 
 // Correct uses
 
+import * as namespaceBaz from './foo';
 export { namedBaz } from './foo';
+
 export * as namespaceBar from './foo.js';
+
 // There is no substitution
 export default namespaceBaz;
