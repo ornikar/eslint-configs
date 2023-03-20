858098e60dadd7160471ca9c2c73d0c83234d28b
Christophe Hurpeau <christophe@hurpeau.com>
2023-03-20 10:15:09 +0100

    feat(eslint-config): unicorn no-array-reduce with configured allowSimpleOperations [no issue] (#493)

--- @ornikar/eslint-config/tests/array-reduce.js
+++ @ornikar/eslint-config/tests/array-reduce.js
@@ -1,6 +1,14 @@
 'use strict';
 
 const array = [];
+
+// Incorrect uses
+
 const reducer = () => {};
 
+// eslint-disable-next-line unicorn/no-array-reduce
 array.reduce(reducer, 0);
+
+// Correct uses
+
+array.reduce((a, b) => a + b, 0);
