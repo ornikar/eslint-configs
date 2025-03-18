f9b8148308cc15dee408031a64fd2d6eb1e97ee1
Paul Sniezak <paul.sniezak@ornikar.com>
2025-03-18 17:54:24 +0100

    feat(eslint-config-typescript-react): activate props rules [no issue]

--- @ornikar/eslint-config-typescript-react/rules/react.js
+++ @ornikar/eslint-config-typescript-react/rules/react.js
@@ -12,14 +12,6 @@ module.exports = {
       },
     ],
 
-    /* disable all prop-types rules */
-    'react/default-props-match-prop-types': 'off',
-    'react/forbid-foreign-prop-types': 'off',
-    'react/forbid-prop-types': 'off',
-    'react/prop-types': 'off',
-    // https://github.com/yannickcr/eslint-plugin-react/issues/2702
-    'react/sort-prop-types': 'off',
-
     // overrides camelcase rule to allow UNSAFE_* commonly used in react libraries
     camelcase: ['error', { ...baseStyleRules.camelcase[1], allow: ['^UNSAFE_'] }],
   },
