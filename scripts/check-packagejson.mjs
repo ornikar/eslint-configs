e20b4cc91f27e5b12c19992d909896887aa121c3
Paul Sniezak <paul.sniezak@ornikar.com>
2026-05-15 18:34:04 +0200

    feat: ignore irrelevant peer dep warning

--- scripts/check-packagejson.mjs
+++ scripts/check-packagejson.mjs
@@ -3,5 +3,13 @@ import { createCheckPackageWithWorkspaces } from 'check-package-dependencies';
 await createCheckPackageWithWorkspaces({
   isLibrary: () => true,
 })
-  .checkRecommended()
+  .checkRecommended({
+    onlyWarnsForInMonorepoPackagesDependencies: {
+      '@ornikar/eslint-config-react': {
+        'eslint-config-airbnb': {
+          invalidPeerDependencyVersion: ['eslint-plugin-react-hooks'],
+        },
+      },
+    },
+  })
   .run();
